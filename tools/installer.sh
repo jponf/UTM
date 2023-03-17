2096be93e8715296cba157ce311590ce25a97d8c
Josep Pon Farreny <josep.pon-farreny@siemens.com>
2023-03-17 10:53:21 +0100

    chore: Updated installer script

--- tools/installer.sh
+++ tools/installer.sh
@@ -18,16 +18,21 @@ else
     platform="unknown"
 fi
 
-pymain=../src/utm.py
+pymain=../utm/__main__.py
 outname=utm-${platform}-${arch_str}
 specdir=installer
 distdir=${specdir}/dist
 workdir=${specdir}/build
 
 
+# Add data separator is ; in windows
 pyinstaller --log-level=INFO \
+    --onefile             \
     --name "${outname}"   \
     --specpath=${specdir} \
     --distpath=${distdir} \
     --workpath=${workdir} \
+    --hidden-import="utm.resources" \
+    --add-data "../../utm/resources/icon.png:utm/resources" \
+    --i ../graphics/icon.ico \
     ${pymain}
